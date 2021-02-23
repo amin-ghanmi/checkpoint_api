f208e39bfc44fb5cf0da7747ec66944ac5c5d94e
amin-ghanmi <med.amin.ghanmi22@gmail.com>
2021-02-23 21:15:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserList from './Components/UserList';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      
+    <UserList/>
+    </div>
+  );
+}
+
+export default App;
